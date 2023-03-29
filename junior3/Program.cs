8cac9fa736c82af03379501a8877c77762409488
Zmeegolov <karantinz@mail.ru>
2023-03-29 19:21:12 +0300

    Добавьте файлы проекта.

---
+++ junior3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Как вас зовут?");
+string name = Console.ReadLine();
+
+Console.WriteLine("Ваш возраст?");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Знак зодиака?");
+string zodiac = Console.ReadLine();
+
+Console.WriteLine("Место работы?");
+string work = Console.ReadLine();
+
+Console.WriteLine($"Вас зовут {name}, ваш возраст {age}, вы {zodiac} по знаку зодиака и вы работаете на {work}");
